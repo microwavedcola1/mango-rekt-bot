fa86c042161870233bc3e23fba1152d6907884b1
microwavedcola1 <microwavedcola@gmail.com>
2022-01-12 13:39:11 +0100

    typo
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -261,6 +261,6 @@ setInterval(updateMangoBaseLots, dayInSeconds * 1000);
 ////////////////
 // kick-off
 
-const twoMinutesSeconds = 1 * 60;
+const oneMinutesSeconds = 1 * 60;
 notifierErrorWrapper();
-setInterval(notifierErrorWrapper, twoMinutesSeconds * 1000);
+setInterval(notifierErrorWrapper, oneMinutesSeconds * 1000);
