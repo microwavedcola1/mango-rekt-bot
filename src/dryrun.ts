e9af84b3c1ec5c133cfe64c55dd96bf7a81cd4dc
microwavedcola1 <microwavedcola@gmail.com>
2022-05-01 07:34:23 +0200

    add new examples, anchor has changed logging prefix, so older examples are invalid
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- src/dryrun.ts
+++ src/dryrun.ts
@@ -3,78 +3,19 @@ import { sleep } from "@blockworks-foundation/mango-client";
 
 async function main() {
   // LiquidatePerpMarket
-  // 2022-04-02 00:55:55
   await processSignature(
-    "26z5Z7bQ1YXp6AXZHcLcrk9xVXropPfNFKnbZvanBkrftJTX8PbSV9Ew8ejQv9ahryWgUWg5PtoZvVcwRmwgWH1D"
+    "4uibVribNbszXEGXcWfqckp1WjmQG8P6RAwy8JiLHEMzBa4LKc9aL3pvx7RrHzLGAWe1pE8Yh4TDanLjAM6Fhmq"
   );
-  // processSignature(
-  //   "5qEhZo7n1gtwkW5Sigjb6e68pSJ3jVjqQ7NXi7jyaNrR458MF2U9i2tPVSmig46mRPEQu1WNM9PLTJigG8ayAy76"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "3129avKVeJBWBrsnB19JpBZcECyJfjmNkEe9vH6KYuDab7aEwqkiQA4xpf3qHxrBnhkNJhhLKiSyTs73agdQ5mTb"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "53eAsAdD49h8A8C32Ticmh3mUvos3dMjh2DM8kWNpoDenmP3aYV3iB5kxVf7N6aUsRtRKfLE222RQBYtodEcqVFU"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "4savWNdm33zjWdzk5v6XzKeRNuuEq1ZK2ZeAB4rpJWh1TtrdskFd9EDoHRRmAuDPjGvRGNWd1Noeu5MGabjJCVvK"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "2MEpA6GkR5R3pyujK36Ujh114651yKvqZcYsRoR4LM5EvZecEgMDcsHzZJoCd4LBnAwFWURBHjWxsNQRfqZMK1uo"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "2gJfqgE8vaVxV1nDz5CJmRrEUq3DCpFbAbRiwA2gdSaui3UAUWMvzLQjnJCDJ4hYMWNjHV7sYg3o6Ua8ZDxApcDP"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "4HtopbXyZNv5v92BgvwHweoJQ5mZomP1Zx3UStVr8mMoLZE6w65TNkr6LCfi9HWixxXuJsFWAd3iKTHNFpHmGFhj"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "4savWNdm33zjWdzk5v6XzKeRNuuEq1ZK2ZeAB4rpJWh1TtrdskFd9EDoHRRmAuDPjGvRGNWd1Noeu5MGabjJCVvK"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "54Hc4JT6k5JHWHDzwppxPR7X3nPx6XSKhGaX3oNs2E6h8KP9AvxMHiUPw9wk26dD2FpijvCpLq4WpvXsADKmqqFU"
-  // );
-  // sleep(1000);
-  // processSignature(
-  //   "3myygdKjassTZAPDQQzhJHdhAUZwbfamPWf12ZrydgqSCJmQ5cGS1zehDDKao22mgaNkrojn4gADRyQ2dUZdKEG3"
-  // );
-  // sleep(1000);
 
   // LiquidateTokenAndToken
-  // processSignature(
-  //   "tc1n66bQMpsPoyxm71QbkvhiXXCEe9xiHhN1Zs3AWPvYmfU2211gRLka2vNUHLZV9sEy8wosQYZuKh3CVpC3yQL"
-  // );
-  // processSignature(
-  //   "2a7W89buGFpbJGT6uKdbWJrUjFSb3pfqAEbnmTRw7qBpZpuBWkR4DXkaGnkeXLb8QtbM53dBPrSWD3fGhHbb217W"
-  // );
-  // processSignature(
-  //   "2fBqNxiREtSYwHeBAQSrZH6692TQLCapcUQ9i5RFQuhEFktsXP22CLHjrgFyWsugvqZG7Ta3BWwd9QgExv7jz8PB"
-  // );
-
-  // LiquidateTokenAndPerp
-  // processSignature(
-  //   "2WYkCZnzxkVcpFgVEBhUpnRYyBNZNHmx8oEpL59UL8KbL7jPftRvaDmjtG7uBfLnANWghsAuuSbQWP9JuYcPCXcg"
-  // );
-  // 2022-04-02 00:54:08
   await processSignature(
-    "2RkybYDjiJ5vjhMf8brv8raE6Ju2XZvZX4nEmMWnqEwmURvzqTkphxAQaEJQshSu6idoNjfxNptr8WTMYnjVJCAC"
+    "5kthnHBYcaoPABjoP6fXqw3pfZ8WhKHR3zyqqGb3vBKdmhBZ4FhmmMpeg7c21wyQoiyPnebo3b1axxLLfLhX2fkb"
   );
 
-  /////// rekt from discord
-  // processSignature(
-  //   "5sokdtBiV5JtETLg3Kpc3qStQbzMD2HAYMQ9TEo5bAFroVNoD85dV26F2Bxmihbrc5QS4tqXcaPwUzwFVvSrW1oe"
-  // );
-
-  /// christian
-  // processSignature("4ZB4oLgvYXnwri3HWDUuaTREa2jfxt6jFjnYKUeRQvoChtEs7fmuw23VB1uCtrJBRhg4DQZZ9DbKoDhNqYwSZ9XB");
+  // LiquidateTokenAndPerp
+  await processSignature(
+    "29gJP3zuiQfjkW5EQccbMFxNjvTY4PBD2SRrQVPF9J6rJcvGs6ZchHaPR9LCaKbFKLFUN2Thgmm84cXK3knRVMBz"
+  );
 
   process.exit();
 }
