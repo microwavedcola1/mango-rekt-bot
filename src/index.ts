8f0cf2a828f49915598f0367220696c12d594eed
microwavedcola1 <microwavedcola@gmail.com>
2022-05-26 12:34:25 +0200

    Revert "Remove discord embedding (#5)"
    This reverts commit e3540ee10f5a87ad700badf5ce04284b7c6773ac.

--- src/index.ts
+++ src/index.ts
@@ -158,10 +158,10 @@ async function notifier() {
     // remove url embedding on discord
     const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     const regex = new RegExp(expression);
-    
-    const urls = [...new Set(combinedNotification.match(regex))];
+
+    const urls = combinedNotification.match(regex);
     for (const url of urls) {
-      combinedNotification = combinedNotification.replaceAll(url, `<${url}>`)
+      combinedNotification = combinedNotification.replace(url, `<${url}>`)
     }
 
     axios.post(process.env.WEBHOOK_URL, { content: combinedNotification });
